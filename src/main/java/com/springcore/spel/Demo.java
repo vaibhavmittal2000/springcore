173c6b9065007c2671c5ac172cdf1c29561459e6
Vaibhav <vaibhavmittal2017@gmail.com>
2024-11-21 13:28:46 +0530

    SpEL complete

--- src/main/java/com/springcore/spel/Demo.java
+++ src/main/java/com/springcore/spel/Demo.java
@@ -9,6 +9,14 @@ public class Demo {
 	private int x;
 	@Value("#{2+5+56+34}")
 	private int y;
+	@Value("#{ T(java.lang.Math).sqrt(25) }")
+	private double z;
+	@Value("#{ T(java.lang.Math).PI }")
+	private double e;
+	@Value("#{new java.lang.String('Vaibhav')}")
+	private String name;
+	@Value("#{8 > 3}")
+	private boolean isActive;
 	
 	public int getX() {
 		return x;
@@ -22,9 +30,28 @@ public class Demo {
 	public void setY(int y) {
 		this.y = y;
 	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
+	public double getE() {
+		return e;
+	}
+	public void setE(double e) {
+		this.e = e;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", name=" + name + ", isActive=" + isActive
+				+ "]";
 	}
 }
